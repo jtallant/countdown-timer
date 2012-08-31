86613f7cf4a03bfb7b78f17e3e069fc675c305a0
Justin Tallant <jtallant07@gmail.com>
2012-08-30 20:38:21 -0500

    jshint countdown.js

--- countdown.js
+++ countdown.js
@@ -1,7 +1,5 @@
-$(document).ready(function() {
-	$('#btn-start').click(doTimer);
-	$('.showVideo').click(toggleVideo);
-});
+/*global $, document, setTimeout, clearTimeout */
+(function() {
 
 var t,
 	currentMin,
@@ -14,16 +12,16 @@ function timedCount() {
 	seconds = seconds - 1;
 	var outStr = minutes + ":" + seconds;
 	if (seconds <= 9) {
-		var outStr = minutes + ":" + "0" + seconds;
+		outStr = minutes + ":" + "0" + seconds;
 	}
 	$('#countdown').html(outStr);
 	$('#elapsedMin').val(minutes);
 	$('#elapsedSec').val(seconds);
-	if (seconds == 0) {
+	if (seconds === 0) {
 		seconds = 60;
 		minutes = minutes - 1;
 	}
-	t=setTimeout("timedCount()",1000);	
+	t = setTimeout(function() { timedCount(); },1000);
 }
 
 function doTimer() {
@@ -32,22 +30,18 @@ function doTimer() {
 	clearTimeout(t);
 	if (!timer_is_on) {
 		$('#btn-start').html('Pause Test');
-		timer_is_on=1;
+		timer_is_on = 1;
 		timedCount();
 	} else {
 		$('#btn-start').html('Start Test');
 		var totalTime = currentMin + ":" + currentSec;
 		$('#countdown').html(totalTime);
-		timer_is_on=0;
+		timer_is_on = 0;
 	}
 }
 
-function toggleVideo() {
-	if ($(this).hasClass('hidden')) {
-		$(this).removeClass('hidden').addClass('visible');
-		$(this).prev('.q-video').slideDown();
-	} else {
-		$(this).removeClass('visible').addClass('hidden');	
-		$(this).prev('.q-video').slideUp();
-	}
-}
+$(document).ready(function() {
+	$('#btn-start').click(doTimer);
+});
+
+})();
